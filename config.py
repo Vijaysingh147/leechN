36007459eba2eef13bd5c2231e43d46321463b31
Vijaysingh147 <161139887+Vijaysingh147@users.noreply.github.com>
2024-03-30 23:11:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,27 +1,27 @@
 import os
 
-API_ID = API_ID = 9741228
+API_ID = API_ID = 23004101
 
-API_HASH = os.environ.get("API_HASH", "6f339e8fa827a5147fa2ff03898d610c")
+API_HASH = os.environ.get("API_HASH", "a2e157e87728053027cbb156e41a832a")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6933526246:AAH4rd7MTUfhIG5lSI4BdwVt47BKG8fVyI0")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 6981453498))
+OWNER = int(os.environ.get("OWNER", 5409975736))
 
-LOG = -1002122751557
+LOG = --1002036952917
 
 try:
   GROUPS =[]
-  for x in (os.environ.get('GROUPS', '-1002025716464 -1002116445692').split()):
+  for x in (os.environ.get('GROUPS', '-4165928800 -100211644569').split()):
     GROUPS.append(int(x))
 except ValueError:
     raise Exception("Your AUTH GROUPS list does not contain valid integers.")    
 
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "809150135 6169271833").split()):
+    for x in (os.environ.get("ADMINS", "5409975736 5442086114").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
